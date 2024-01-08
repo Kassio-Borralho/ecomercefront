692e07174b5b53ac84d5f6845acc9252774b8c8e
Kassio Borralho <kassioborralho@gmail.com>
2024-01-08 18:15:58 -0300

    atualização vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductView from '../views/ProductView.vue'
+import ButtonView from '@/views/ButtonView.vue'
 
 
 const router = createRouter({
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/produto',
       name: 'produto',
       component: ProductView
+    },
+    {
+      path: '/demostracaobtn',
+      name: 'buttons',
+      component: ButtonView
     }
 
   ]
---
+++ src/views/ButtonView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import BtnComprar from '@/components/app/buttons/BtnComprar.vue';
+
+</script>
+
+<template>
+    <BtnComprar>Enviar</BtnComprar>
+</template>
