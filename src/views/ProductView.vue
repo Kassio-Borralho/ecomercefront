18e7187c8bfaa7a6c7b559b5f5e4a1935fc12e02
Kassio Borralho <kassioborralho@gmail.com>
2024-01-08 19:53:28 -0300

    criação da view produto

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,9 +1,31 @@
 <script setup>
 import AppLayout from '@/layout/AppLayout.vue';
+import Pneu from "@/assets/images/pneu.png";
+import BtnComprar from '@/components/app/buttons/BtnComprar.vue';
 </script>
 <template>
     <AppLayout>
-
-        detalhes do produto
+        <div class="flex items-center bg-gray-100">
+            <div class="">
+        <img class="bject-contain h-42 w-96  " :src="Pneu" >
+       </div>
+       <div>
+        <p class="mt-6">
+            XBR
+        </p>
+        <p class="font-bold text-lg ">
+            Pneu para Carro 175/70 Aro 13 82T Premium F7 Xbri
+        </p>
+        <p class="mt-5">
+            1 Unidades
+        </p>
+        <p class="mt-6"> 
+            R$ 200,90 
+        </p>
+       <div class="mt-10"> <BtnComprar >Adicionar</BtnComprar></div>
+    </div>
+        </div>
+       
+       
     </AppLayout>
 </template>
